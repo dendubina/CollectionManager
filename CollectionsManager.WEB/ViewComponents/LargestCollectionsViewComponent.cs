d60537db67abac8ff0a021e6034c03c5beee20c4
RapidRapidRabbit <awestruck31@mail.ru>
2022-10-18 12:06:20 +0300

    largest collections view component

---
+++ CollectionsManager.WEB/ViewComponents/LargestCollectionsViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using CollectionsManager.BLL.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollectionManager.WEB.ViewComponents
+{
+    public class LargestCollectionsViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public LargestCollectionsViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+            => View("LargestCollectionsView", await _unitOfWork.Collections.GetMostLargeCollectionsAsync(5));
+    }
+}
